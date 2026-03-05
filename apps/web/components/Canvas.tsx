d5d25a8a3dc43671c39e5660326b2ce1f0a61d86
JITHIN B P <jithinbp007@gmail.com>
2026-03-05 17:46:34 +0530

    feat(web): implement high dpi canvas scale

--- apps/web/components/Canvas.tsx
+++ apps/web/components/Canvas.tsx
@@ -1,10 +1,39 @@
 'use client';
 
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+
+    const canvasResize = () => {
+      const parent = canvas.parentElement;
+      if (!parent) return;
+      const rec = parent.getBoundingClientRect();
+
+      const dpr = window.devicePixelRatio || 1;
+
+      canvas.width = rec.width * dpr;
+      canvas.height = rec.height * dpr;
+
+      canvas.style.width = `${rec.width}px`;
+      canvas.style.height = `${rec.height}px`;
+
+      ctx.setTransform(dpr, 0, 0, dpr, 0, 0);
+    };
+
+    canvasResize();
+
+    window.addEventListener('resize', canvasResize);
+
+    return () => window.removeEventListener('resize', canvasResize);
+  });
   return (
     <div className="absolute inset-0">
       <canvas ref={canvasRef} className="w-full h-full block cursor-crosshair" />
